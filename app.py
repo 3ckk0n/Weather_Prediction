1a7d077d8c4cd9b1936b916125292f5300b04377
3ckkon <21521948@gm.uit.edu.vn>
2024-11-27 21:17:08 +0700

    page setup
    using streamlit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Rain prediction",page_icon=":umbrella_with_rain_drops:", layout ="wide")
+
+#Header
+st.subheader("Rain prediction web application :umbrella_with_rain_drops:")
+st.title("By Pham Tien Dat and Nguyen Tran Hoi Thang")
+st.write("Testing")
+st.write("[Learn more>](https://www.facebook.com/nguyenmoevl)")
